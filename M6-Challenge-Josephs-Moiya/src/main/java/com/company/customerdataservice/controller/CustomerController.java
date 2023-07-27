8f6d67d3dd7572f6b35083f33560bed04cd747cb
moiyajosephs <josephsmoiya@gmail.com>
2023-07-26 21:00:20 -0400

    add customer contr and repo

--- M6-Challenge-Josephs-Moiya/src/main/java/com/company/customerdataservice/controller/CustomerController.java
+++ M6-Challenge-Josephs-Moiya/src/main/java/com/company/customerdataservice/controller/CustomerController.java
@@ -1,16 +1,51 @@
 package com.company.customerdataservice.controller;
 
-public class CustomerController {
-//    A POST route that creates a new customer. 10 pts
+import com.company.customerdataservice.model.Customer;
+import com.company.customerdataservice.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerController {
+    @Autowired
+    CustomerRepository repo;
+//    A POST route that creates a new customer. 10 pts
+    @PostMapping("/customers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer addCustomer(@RequestBody Customer customer ){
+        return repo.save(customer);
+    }
 //
 //    A PUT route that updates an existing customer. 10 pts
+    @PutMapping("/customers")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Customer updateCustomer(@RequestBody Customer customer){
+        return repo.save(customer);
+    }
 //
 //    A DELETE route that deletes an existing customer. 10 pts
+    @DeleteMapping("/customers/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable int id){
+        repo.deleteById(id);
+    }
 //
 //    A GET route that returns a specific customer by id. 10 pts
+    @GetMapping("/customers/{id}")
+    public Customer getCustomerById(@PathVariable int id){
+        Optional<Customer> returnedCustomer = repo.findById(id);
+        return returnedCustomer.orElse(null);
+    }
+
 //
 //    A GET route that returns all customers for a specific state. 10 pts
+    public List<Customer> getCustomersByState(@PathVariable String state){
+        return repo.findByState(state);
+    }
 
 
 }
--- M6-Challenge-Josephs-Moiya/src/main/java/com/company/customerdataservice/repository/CustomerRepository.java
+++ M6-Challenge-Josephs-Moiya/src/main/java/com/company/customerdataservice/repository/CustomerRepository.java
@@ -4,6 +4,8 @@ import com.company.customerdataservice.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     // Create a new customer record. 5 pts
@@ -15,4 +17,5 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     //    Find a customer record by id. 5 pts
 
     //    Find customer records by state. 5 pts
+    List<Customer> findByState(String state);
 }
