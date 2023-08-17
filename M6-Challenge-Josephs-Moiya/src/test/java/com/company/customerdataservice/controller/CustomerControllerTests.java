f2d0fb3ea11097c4c57f7d63ef9922e226ca0aa9
Moiya Josephs <josephsmoiya@gmail.com>
2023-08-17 18:25:05 -0400

    ad m9 and m6 challenge

--- M6-Challenge-Josephs-Moiya/src/test/java/com/company/customerdataservice/controller/CustomerControllerTests.java
+++ M6-Challenge-Josephs-Moiya/src/test/java/com/company/customerdataservice/controller/CustomerControllerTests.java
@@ -19,7 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CustomerController.class)
-
 public class CustomerControllerTests {
     @Autowired
     private MockMvc mockMvc;
